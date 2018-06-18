9b5b95a0848daa9b1d4852c30d0125bf96ccca19
유성덕 <sungduk.yu@navercorp.com>
2018-06-18 18:45:08 +0900

    commit init code

---
+++ __tests__/client.test.js
@@ -0,0 +1,47 @@
+const ncloud = require('../lib/ncloud');
+const Server = require('server');
+
+const n = new ncloud();
+
+const client = new Server.ApiClient(n.keys());
+const instance = new Server.V1Api(client);
+
+test('getLoginKeyListGet', done => {
+  instance.getLoginKeyListGet({ keyName: 'rabbit' }, function(err, data) {
+    expect(err).toBeNull();
+    expect(data).not.toBeNull();
+    expect(data.totalRows).toBe(1);
+    expect(data.loginKeyList[0].keyName).toBe('rabbit');
+    done();
+  });
+});
+
+test('getLoginKeyListPost', done => {
+  instance.getLoginKeyListPost({ keyName: 'rabbit' }, function(err, data) {
+    expect(err).toBeNull();
+    expect(data).not.toBeNull();
+    expect(data.totalRows).toBe(1);
+    expect(data.loginKeyList[0].keyName).toBe('rabbit');
+    done();
+  });
+});
+
+test('getPublicIpInstanceListGet', done => {
+  instance.getPublicIpInstanceListGet({ publicIpInstanceNoList: ['691030', '690456', '690457'] }, function(err, data) {
+    expect(err).toBeNull();
+    expect(data).not.toBeNull();
+    expect(data.totalRows).toBe(3);
+    expect(data.publicIpInstanceList[0].publicIpInstanceNo).toBe('690456');
+    done();
+  });
+});
+
+test('getPublicIpInstanceListPost', done => {
+  instance.getPublicIpInstanceListPost({ publicIpInstanceNoList: ['691030', '690456', '690457'] }, function(err, data) {
+    expect(err).toBeNull();
+    expect(data).not.toBeNull();
+    expect(data.totalRows).toBe(3);
+    expect(data.publicIpInstanceList[0].publicIpInstanceNo).toBe('690456');
+    done();
+  });
+});
