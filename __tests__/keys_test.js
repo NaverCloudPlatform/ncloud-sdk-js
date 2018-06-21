a5347722240b6f4aa607172148f93f75cf295ddb
유성덕 <sungduk.yu@navercorp.com>
2018-06-21 20:47:42 +0900

    - spec changed : to not use apiKey

--- __tests__/keys_test.js
+++ __tests__/keys_test.js
@@ -1,10 +1,9 @@
 test('get keys', () => {
   const ncloud = require('../lib/ncloud');
-  const options = { apiKey: 'a', accessKey: 'b', secretKey: 'c' };
+  const options = { accessKey: 'b', secretKey: 'c' };
   const n = new ncloud(options);
   const keys = n.keys();
 
-  expect(keys.apiKey).toBe(options.apiKey);
   expect(keys.accessKey).toBe(options.accessKey);
   expect(keys.secretKey).toBe(options.secretKey);
 });
